a56c307e49847a140130090f44bb2bd38dc88e6b
Renan Paulino <dev.renanpaulino@gmail.com>
2026-08-17 19:22:42 -0300

    feat: estrutura de pasta aulas

--- OlaMundo.dart
+++ OlaMundo.dart
@@ -3,3 +3,9 @@
 /*
 comentário de bloco
 */
+
+void main() {
+  // A função print() exibe uma mensagem de texto no console
+  // O texto entre aspas simples é o chamanto "String" ou cadeia de caracteres
+  print("Olá dart!");
+}
