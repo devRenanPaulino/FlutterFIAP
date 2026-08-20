6d59823122c33992b3583dd51c254e5f78170ed6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-19 22:44:28 -0300

    feat: aula 3

---
+++ 002_AULA/Introducao/003-Desafio/interpolacao.dart
@@ -0,0 +1,8 @@
+void main() {
+  String nome = "Renan";
+  int idade = 19;
+  double altura = 1.73;
+  String cidade = "São Paulo";
+
+  print("Meu nome é $nome, tenho $idade, $altura m de altura e moro em $cidade");
+}
---
+++ 003_AULA/linhadecorativa.dart
@@ -0,0 +1,23 @@
+//Desafio: criar um cartão de apresentação usando apenas saídas do console
+// O objetivo é praticar sequências de prints para montar uma saida formatada
+
+void main() {
+  // linha decorativa que delimita o inicio do cartão
+  print("=" * 70);
+  print("Catão de Apresentação - DART");
+  print("=" * 70);
+  // Dados pessoais do desenvolvedor(a) em formação
+  print("Nome: Renan");
+  print("Área de Interesse: Arquitetura de Software");
+  print("Motivo: Pelo Dinheiro");
+  //linha em branco
+  print("");
+  print("Metas");
+  print("1. Praticar dart todos os dias");
+  print("2. Criar meu primeiro programa em dart");
+  print("3. Publicar um programa do github");
+
+  print("");
+  print("Menssagem: Acredite no processo e na jornada será sucesso!");
+  print("=" * 70);
+}
