75f8af879aa0e056b49cfb5ae2e1540b4cce910b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-26 21:15:18 -0300

    feat: ex03 funcao

---
+++ Introducao/004_AULA_FUNCOES/003_Desafio/ex_03.dart
@@ -0,0 +1,18 @@
+/*
+  **Filtro Customizado (Closure):** Crie uma função `criarFiltroMaiorQue(double limite)` que retorna uma função anônima. A função retornada deve receber um número e indicar se ele é maior que o `limite`. Teste com uma lista de valores.
+*/
+
+Function criarFiltroMaiorQue(double limite) {
+  return (double numero) => numero > limite;
+}
+
+void main() {
+  List<double> lista = [1.0, 2.5, 3.0, 4.2, 5.0];
+
+  var filtroMaiorQueDoisEMeio = criarFiltroMaiorQue(2.5);
+
+  List<double> resultado = lista.where((num) => filtroMaiorQueDoisEMeio(num)).toList();
+
+  print(resultado);
+  
+}
